c47771916a024f83ed8b886ab7b2a70c4a2f412f
Shadhanan S <shadhanan.project@gmail.com>
2026-08-06 11:40:52 +0530

    Time: 3 ms (17.48%), Space: 19.3 MB (32.93%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
@@ -5,7 +5,6 @@ class Solution:
             prd=1
             for i in str(a):
                 prd*=int(i)
-            # print(a)
             if(prd%t==0):
                 return a
                 break
