4a1a20456249d19a115af15997e7711cdaffdd3e
Shadhanan S <shadhanan.project@gmail.com>
2026-08-11 06:15:53 +0000

    Time: 13 ms (10.55%), Space: 19.8 MB (38.4%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        a=s.lower()
+        # s.lower()
+        stri=""
+        for i in range(len(s)):
+            if(a[i]>='a' and a[i]<='z' or (a[i]>='0' and a[i]<='9')):
+                stri+=a[i]
+            else:
+                continue
+        print(stri)
+        return (stri==stri[::-1])
+        # i=0
+        # j=len(stri)-1
+        # c=1
+        # while(i!=j):
+        #     if(stri[i]==stri[j]):
+        #         i+=1
+        #         j-=1
+        #         c=1
+        #     else:
+        #         c=0
+        #         break
+        # if(c):
+        #     return True
+        # else:
+        #     return False
